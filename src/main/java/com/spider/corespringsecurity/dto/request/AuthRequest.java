ddc355a7b3ddf9b5b150a595aebfb29ce8619bd4
rajat.gupta <rajat.gupta@flexcar.com>
2023-08-15 12:47:48 +0530

    added validations

--- src/main/java/com/spider/corespringsecurity/dto/request/AuthRequest.java
+++ src/main/java/com/spider/corespringsecurity/dto/request/AuthRequest.java
@@ -1,5 +1,6 @@
 package com.spider.corespringsecurity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+    @NotBlank
     private String username ;
+
+    @NotBlank
     private String password;
 }
